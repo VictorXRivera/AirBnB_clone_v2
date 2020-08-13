02e6361d43d3ac5b47b76a2d7ebbcd42431c4fbb
Kathleen McKiernan <kathleenmckiernan@UofNs-iMac-3.local>
2020-08-13 16:26:56 -0400

    update

--- models/user.py
+++ models/user.py
@@ -3,15 +3,23 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel, Base):
     """class for user
     Project 7 - Specify string length for attributes to 128
               - Names can be null, but email and password can't
+              - Added check in database
     """
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
