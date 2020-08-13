79714df2c92299cfd925db4b67be14702727c77a
Kathleen McKiernan <kathleenmckiernan@UofNs-iMac-3.local>
2020-08-13 14:56:15 -0400

    update user with string length

--- models/user.py
+++ models/user.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    """
+    Project 7 - Specify string length for attributes to 128
+              - Names can be null, but email and password can't
+    """
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
