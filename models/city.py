cd36a0c6bc0dcf952ab56584a15c7ebd5e6bb734
VictorXRivera <victorxrivera.professional@gmail.com>
2020-08-13 23:59:51 +0000

    Task 6

--- models/city.py
+++ models/city.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
 
 
 class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    """ The city class, contains state ID and name
+    However if a state is deleted then the city associated
+    with the state will be deleted as well
+    """
+
+    __tablename__ = 'cities'
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship('Place', cascade='all, delete-orphan',
+                            backref='cities')
+    else:
+        name = ""
+        state_id = ""
--- models/state.py
+++ models/state.py
@@ -5,4 +5,20 @@ from models.base_model import BaseModel
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship('City',
+                cascade="all, delete-orphan",
+                backref="state")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """ Cities method """
+            city_item = []
+            for item in models.storage.all(City).values():
+                if item.state_id == self.id:
+                    city_item.append(item)
+            return city_item
