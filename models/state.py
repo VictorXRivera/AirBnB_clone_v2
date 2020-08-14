7c608b8c370f678a3486139f08d0bd03eb6cc3bd
VictorXRivera <victorxrivera.professional@gmail.com>
2020-08-14 00:12:04 +0000

    Fixed import issue with state.py

--- models/state.py
+++ models/state.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from models.base_model import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from models.city import City
+import os
 
 
 class State(BaseModel):
