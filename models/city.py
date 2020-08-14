8388a21be600047ba787147541f8f69bb28f8688
Kathleen McKiernan <kathleenmckiernan@UofNs-iMac-3.local>
2020-08-13 22:07:18 -0400

    sync with remote for checker

--- models/city.py
+++ models/city.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    """ The city class, contains state ID and name 
+    Project 8 - Create relationship between city and places for deletion
+    """
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship("Place", backref="cities", cascade="all, delete")
+    else:
+        state_id = ""
+        name = ""
