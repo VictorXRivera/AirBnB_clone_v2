3fce6b42e14a4632255dc34d80f8868da880611c
VictorXRivera <victorxrivera.professional@gmail.com>
2020-08-18 15:36:35 +0000

    Fabric script to pack web static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive from the contents of 
-the web_static folder of your AirBnB Clone repo, using the function do_pack"""
+"""
+Fabric script that generates a .tgz archive from the contents of 
+the web_static folder of your AirBnB Clone repo, 
+using the function do_pack
+"""
 
 
 def do_pack():
