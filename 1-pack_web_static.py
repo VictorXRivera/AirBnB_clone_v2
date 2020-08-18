6a91fecd1ed8a30958ad206610292010bfa2053f
VictorXRivera <victorxrivera.professional@gmail.com>
2020-08-18 15:54:31 +0000

    Made changes to python script

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,17 +4,29 @@ Python script that generates a .tgz archive from the contents of
 the web_static folder of my AirBnB Clone repo, 
 using the function do_pack
 """
-from fabric.api import *
-from datetime import datetime
-import os
 
 
 def do_pack():
-    """    
-    The function do_pack must return the archive path 
-    if the archive has been correctly generated. 
+    """
+    ---------------
+    Method: do_pack
+    ---------------
+    do_pack will generate a .tgz archive from the
+    contents of the web_static folder.
+
+    Requirements:
+    - All files in the folder web_static must be added to the final archive
+    - All archives must be stored in the folder versions
+    (the function should create this folder if it doesnt exist)
+    - The name of the archive created must be
+    web_static_<year><month><day><hour><minute><second>.tgz
+    -The function do_pack must return the archive
+     path if the archive has been correctly generated. 
     Otherwise, it should return None
     """
+    from fabric.api import local
+    from datetime import datetime as time
+
     file = "web_static_" + time.now().strftime("%Y%m%d%H%M%S") + ".tgz"
 
     local('mkdir -p versions')
