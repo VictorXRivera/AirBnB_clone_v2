6f1cba6a27ca5e55287e66acd964ac0180bed4fb
VictorXRivera <victorxrivera.professional@gmail.com>
2020-08-19 01:34:53 +0000

    Trying to fix pep8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,29 +1,9 @@
 #!/usr/bin/python3
-"""
-Python script that generates a .tgz archive from the contents of 
-the web_static folder of my AirBnB Clone repo, 
-using the function do_pack
-"""
+"""Python script that generates a .tgz archive"""
 
 
 def do_pack():
-    """
-    ---------------
-    Method: do_pack
-    ---------------
-    do_pack will generate a .tgz archive from the
-    contents of the web_static folder.
-
-    Requirements:
-    - All files in the folder web_static must be added to the final archive
-    - All archives must be stored in the folder versions
-    (the function should create this folder if it doesnt exist)
-    - The name of the archive created must be
-    web_static_<year><month><day><hour><minute><second>.tgz
-    -The function do_pack must return the archive
-     path if the archive has been correctly generated. 
-    Otherwise, it should return None
-    """
+    """do_pack function"""
     from fabric.api import local
     from datetime import datetime as time
 
