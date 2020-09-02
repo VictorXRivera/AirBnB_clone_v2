2b6242d6c7fadb96896509e8b96f43e6646dc41a
VictorXRivera <victorxrivera.professional@gmail.com>
2020-09-02 17:43:28 +0000

    Number route script

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Making a Flask script """
+from flask import Flask, escape, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_route(text):
+    text = text.replace('_', ' ')
+    return "C %s" % text
+
+
+@app.route('/python', defaults={'text': None}, strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_route(text):
+    if text is None:
+        text = 'is cool'
+    text = text.replace('_', ' ')
+    return "Python %s" % text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    return "%d is a number" % n
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
