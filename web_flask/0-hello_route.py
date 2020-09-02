c71fef1d506925c08cb908d511ecab201b57af18
VictorXRivera <victorxrivera.professional@gmail.com>
2020-09-02 02:16:34 +0000

    Made more changes to script

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """ Making a Flask script """
-
 from flask import Flask, escape, request
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
-        return 'Hello HBNB!'
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
