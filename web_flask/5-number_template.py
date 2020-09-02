d2c39ad7f93398acd0cfa2f10b0aea8d022a42d7
VictorXRivera <victorxrivera.professional@gmail.com>
2020-09-02 20:12:45 +0000

    Different method call

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,7 +37,7 @@ def number_route(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
+def num_render_template(n):
     return render_template("5-number.html", n=n)
 
 
