98f66aab4013817114da09518837ecdaa1aa458f
VictorXRivera <victorxrivera.professional@gmail.com>
2020-09-03 00:31:21 +0000

    Made corrections

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,13 @@
 -- Project 3 - Script that prepares a MySQL server for project
 -- with specified instructions
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Creating database
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- ^^^Creates database and user with specific password here
-GRANT ALL PRIVILEGES IN hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 -- ^^^Assign all privileges to hbnb_dev only on hbnb_dev_db
-FLUSH PRIVILEGES;
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 -- ^^^Clears privileges
-GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- ^^^Reassigns privileges to SELECT for hbnb_dev on performance_schema
